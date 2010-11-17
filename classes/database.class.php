3d6eb8c4b71fa1a6756202be8eee43d9bbf0d063
John Heimkes IV <admin@johnheimkes.com>
2010-11-16 18:50:02 -0600

    initial commit

---
+++ classes/database.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database class for goodfuckingdatingadvice.com
+ * @author John Heimkes IV <johnh@ang.st>
+ * @author Jake Dahn <jake@ang.st>
+ * @version 1.0
+ */
+class database {
+  
+  function __construct(argument) {
+    # code...
+  }
+}
