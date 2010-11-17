3d7dfb07478f66fd868b777ce9e9688a38a7f080
John Heimkes IV <admin@johnheimkes.com>
2010-11-17 02:20:01 -0600

    writing some good fucking markup

--- index.php
+++ index.php
@@ -1,48 +1,37 @@
 <?php
-    include "config.inc.php";
-    require_once "classes/class.fucking_db.php";
-        
-    $db = new FuckingDB($config);
-    
-    echo "<pre>";
-    print_r($db->all());
-    echo "</pre>";
-    
-    echo "<pre>";
-    print_r($db->random());
-    echo "</pre>";
+  include "config.inc.php";
+  require_once "classes/class.fucking_db.php";
 
-
-    echo "<pre>";
-    print_r($db->find(3));
-    echo "</pre>";
-    
+  $db = new FuckingDB($config);
+  $tip = $db->random();
 ?>
 <!DOCTYPE html>
 <html lang="en" xml:lang="en">
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>title</title>
-    <meta name="Author" content="Author" />
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <title>Good Fucking Dating Advice.</title>
     <meta name="Robots" content="index,all" />
-    <meta name="Keywords" content="Key Words" />
-    <meta name="Description" content="Descritpion" />
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+    <meta name="Keywords" content="good, fucking, dating, advice" />
+    <meta name="Description" content="Good Fucking Dating Advice &mdash; Your guide to dating since two thousand fucking ten." />
   </head>
   <body>
     <div id="wrapper">
       <div id="header">
         <h1>Good Fucking Dating Advice</h1>
         <ul id="nav">
-          <li><a href="#">Nav Item 1</a></li>
-          <li><a href="#">Nav Item 2</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Family Fucking Friendly</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
       </div>
       <div id="content">
-        
+        <div class="advice">
+          <h2><?= $tip["body"] ?></h2>
+        </div>
       </div>
       <div id="footer">
-        footer
+        <p class="copy">&copy; <?= date("Y")?> John Heimkes IV &mdash; Inspired by <a href="http://goodfuckingdesignadvice.com" target="_blank" title="Good Fucking Design Advice.">this</a>.</p>
       </div>  
     </div>
   </body>
