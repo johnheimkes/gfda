d70f86b79f445a34dc50bafd37d4212582a75385
Jake Dahn <admin@jakedahn.com>
2010-11-24 04:13:56 -0600

    made the fucking friendly version work

--- friendly.php
+++ friendly.php
@@ -1,7 +1,19 @@
+<?php
+  include "config.inc.php";
+  require_once "classes/class.fucking_db.php";
+
+  $db = new FuckingDB($config);
+  
+  $tip = str_ireplace("fucking", "<span class='friendly'>fucking</span>", $db->random());
+?>
+
 <?php 
     include "header.php" 
 ?>
 
+        <div class="advice <?=$tip["gender"]?>">
+          <a href="/show/<?= $tip["id"] ?>"><h2><?= $tip["body"] ?></h2></a>
+        </div>
         
 <?php 
     include "footer.php" 
