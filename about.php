58bf80852bd09f919f8cc328b6c59a55439502f2
Jake Dahn <admin@jakedahn.com>
2010-11-24 03:29:53 -0600

    styling the fucking about page

--- about.php
+++ about.php
@@ -1,8 +1,20 @@
 <?php 
     include "header.php" 
 ?>
-
+        <img id="sad_jake" src="public/images/sad_jake.jpg" width="400" height="554" alt="Sad Jake" />
+        <p class="caption">Rachel, this site was made for me for you :)</p>
         
+        <div id="about">
+          <h3>What the fuck is this?</h3>
+          
+          <p>We all have that friend. Fucking scruffy neck beard, beady pirate eyes, buzzed developer haircut, and desperately in need of bad dating advice - or so we fucking thought.</p>
+
+          <p>In the dead of night, during the fucking great blizzard of the century, Jake Dahn crept out of bed and stepped into the fucking internet. That in it of itself should have been a sign to us that he is more than just a man with an addiction to string cheese - he had been secretly hoarding a ton of good fucking dating advice.</p>
+          
+          <p>and now jake dahn is offering the worlds greatest fucking dating advice - but don't let that stop you from sending in your own tips for the hip young kids out there trying to get past first base while we're here in the internet.</p>
+          
+          
+        </div>
 <?php 
     include "footer.php" 
 ?>
