aff0c3cda667699cc978afab9c0121325c2c915a
John Heimkes IV <admin@johnheimkes.com>
2010-11-24 02:38:44 -0600

    that last commit was so fucking big i had to have another one... bitches

---
+++ footer.php
@@ -0,0 +1,16 @@
+        <div class="separator"></div>
+      </div>
+      <div id="footer">
+        <h3>More Fucking Information</h3>
+        <ul id="nav">
+          <li><a href="#">WTF is this?</a></li>
+          <li><a href="#">Youngin' Fucking Friendly</a></li>
+          <li><a href="#">More Fucking Resources</a></li> <!-- daytime talk show hosts (oprah, maury, dr. phil, montel, etc...) -->
+          <li><a href="#">Fucking Contact Us</a></li>
+        </ul>
+        
+        <p class="copy">&copy; <?= date("Y")?> MarkupIsArt &mdash; Inspired by <a href="http://goodfuckingdesignadvice.com" target="_blank" title="Good Fucking Design Advice.">this</a>.</p>
+      </div>  
+    </div>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" xml:lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <title>Good Fucking Dating Advice.</title>
+    <meta name="Robots" content="index,all" />
+    <meta name="Keywords" content="good, fucking, dating, advice" />
+    <meta name="Description" content="Good Fucking Dating Advice &mdash; Your guide to dating since two thousand fucking ten." />
+    <link rel="stylesheet" href="/public/css/style.css" type="text/css" media="screen" charset="utf-8" />
+  </head>
+  <body>
+    <div id="wrapper">
+      <div id="header">
+        <div id="filter">
+          <h2 id="his"><a href="/his">His</a></h2>
+          <h2 id="hers"><a href="/hers">Hers</a></h2>
+        </div>
+        <h1><a href="#">Good Fucking Dating Advice</a></h1>
+      </div>
+      <div id="content">
+        <div class="separator"></div>
